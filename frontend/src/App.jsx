3c64ea29bbb6ef7e0e0923b4c6fd4993da9f21d4
Poojanpatel2003 <poojanpatel1595@gmail.com>
2025-05-05 10:57:59 +0530

    filter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import PlaceOrder from './pages/PlaceOrder';
 import Order from './pages/Order';
 import Navbar from './components/Navbar';
 import React from 'react';
+import Footer from './components/Footer';
 const App=()=> {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
@@ -27,7 +28,7 @@ const App=()=> {
 
          
       </Routes>
-
+      <Footer/>
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import LatestCollections from '../components/LatestCollections';
+import BestSeller from '../components/BestSeller';
+import OurPolicy from '../components/OurPolicy';
+import NewsLetter from '../components/NewsLetter';
 
 const Home = () => {
     return (
-        <div>
+        <div>   
             <Hero/>
             <LatestCollections/>
+            <BestSeller/>
+            <OurPolicy/>
+            <NewsLetter/>
         </div>
     );
+    
 }
 
 export default Home;
