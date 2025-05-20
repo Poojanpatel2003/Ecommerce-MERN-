bdcd613959171369ab6f4e534df2d14b4e83455e
Poojanpatel2003 <poojanpatel1595@gmail.com>
2025-05-20 10:58:49 +0530

    middleware

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -31,7 +31,7 @@ const addProduct=async(req,res)=>{
         console.log(productData)
         const product=new productModel(productData);
         await product.save();
-        res.json({success:true,message:"Prodcu Added"})
+        res.json({success:true,message:"Product Added"})
     } catch (error) {
         console.log(error)
         res.json({success:false,message:error.message})
@@ -40,17 +40,38 @@ const addProduct=async(req,res)=>{
 
 //list product
 const listProducts=async(req,res)=>{
-    
+    try {
+        const products=await productModel.find({});
+        res.json({success:true,products})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
 }
 
 //removing product
 const removeProduct=async(req,res)=>{
-
+    try {
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({success:true,message:"Product Removed"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
 }
 
 //single product info
 const singleProduct=async(req,res)=>{
+    try {
+        const {productId}=req.body;
+        const product=await productModel.findById(productId);
+        res.json({success:true,product})
 
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message});
+         
+    }
 }
 
 export {listProducts,removeProduct,singleProduct,addProduct}
