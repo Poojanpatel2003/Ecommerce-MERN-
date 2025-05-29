6a0b9af9884693a679e03bb49c3e8ec72772d8bf
Poojanpatel2003 <poojanpatel1595@gmail.com>
2025-05-29 12:49:55 +0530

    orderadmin page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -13,7 +13,7 @@ const placeOrder= async(req,res)=>{
             payment:false,
             date:Date.now()
         }
-        const newOrder=new orderModel(orderModel)
+        const newOrder=new orderModel(orderData)
         await newOrder.save()
         await userModel.findByIdAndUpdate(userId,{cartData:{}})
         res.json({success:true,message:"order Placed"})
@@ -31,12 +31,25 @@ const placeOrderRazorpay= async(req,res)=>{
 
 
 const allOrders= async(req,res)=>{
-
+    try {
+        const orders =await orderModel.find({})
+        res.json({success:true,orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 }
 
 
 const userOrders= async(req,res)=>{
-
+    try {
+        const {userId}=req.body;
+        const orders=await orderModel.find({userId});
+        res.json({success:true,orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 }
 
 
